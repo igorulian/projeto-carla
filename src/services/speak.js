b2c9c6fc2d6c8ce041fe9bf624b823f43c54c619
Igor Ulian <igorulian@Outlook.com>
2021-03-10 17:51:30 -0300

    Adicionei o text to speech do google
    Ainda falta conseguir fazer o audio gerado tocar, e de preferencia fazer esse sistema todo de gerar o audio mais rapido

--- src/services/speak.js
+++ src/services/speak.js
@@ -1,51 +1,90 @@
 const fs = require('fs')
 const dev = require('../services/log')
+const textToSpeech = require('@google-cloud/text-to-speech');
+const util = require('util');
+
+const client = new textToSpeech.TextToSpeechClient();
 
 require('dotenv').config()
-
-const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
-
-// https://www.youtube.com/watch?v=rSjLe8k5DX0  https://cloud.ibm.com/apidocs/text-to-speech?code=node#data-collection
-
-const textToSpeech = new TextToSpeechV1({
-  authenticator: new IamAuthenticator({
-  apikey: "3383ucHfHBmBe1nhrOfSpIVOxUHRWBz-jfChAxdbW3sO",
-  }),
-  serviceUrl: "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/12575fac-d9f8-46c8-a251-f939cbfa69fa",
-  disableSslVerification: true, 
-});
 
 module.exports = {
     async say(text) {
 
-      dev.log('Gerando audio...')
-
-      const synthesizeParams = {
-        text: '... ' + text + '',
-        accept: 'audio/mp3',
-        voice: 'pt-BR_IsabelaVoice'
-      };
-      
-      await textToSpeech.synthesize(synthesizeParams)
-        .then(response => {
-          // only necessary for wav formats,
-          // otherwise `response.result` can be directly piped to a file
-          return textToSpeech.repairWavHeaderStream(response.result);
-        })
-        .then(buffer => {
-          fs.writeFileSync('./src/audio/audio.mp3', buffer);
-        })
-        .catch(err => {
-          console.log('error:', err);
-        });
-
-        dev.log('Audio gerado!')
+        await gerarAudioGoogle(text)
 
         // Fazer tocar o audio
-        
+        dev.log('Tocando...')
+        var audio = new Audio('./src/audio/audio.mp3')
+        audio.play()
         dev.log('Audio tocado')
       
     
     }
 }
+
+
+async function gerarAudioGoogle(text) {
+  // The text to synthesize
+  dev.log('Gerando audio com google Cloud...')
+
+  // Construct the request
+  const request = {
+    input: {text: text},
+    // Select the language and SSML voice gender (optional)
+    voice: {languageCode: 'pt-BR', ssmlGender: 'NEUTRAL'},
+    // select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+
+  // Performs the text-to-speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('./src/audio/audio.mp3', response.audioContent, 'binary');
+
+  console.log('Audio GOOGLE Gerado!');
+}
+
+
+async function gerarAudioIBM(text){
+    dev.log('Gerando audio com IBM Cloud...')
+    const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    // https://www.youtube.com/watch?v=rSjLe8k5DX0  https://cloud.ibm.com/apidocs/text-to-speech?code=node#data-collection
+
+    const textToSpeech = new TextToSpeechV1({
+      authenticator: new IamAuthenticator({
+      apikey: "3383ucHfHBmBe1nhrOfSpIVOxUHRWBz-jfChAxdbW3sO",
+      }),
+      serviceUrl: "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/12575fac-d9f8-46c8-a251-f939cbfa69fa",
+      disableSslVerification: true, 
+    });
+
+    const synthesizeParams = {
+      text: '... ' + text + '',
+      accept: 'audio/mp3',
+      voice: 'pt-BR_IsabelaVoice'
+    };
+    
+    await textToSpeech.synthesize(synthesizeParams)
+      .then(response => {
+        return textToSpeech.repairWavHeaderStream(response.result);
+      })
+      .then(buffer => {
+        fs.writeFileSync('outputIBM.mp3', buffer);
+      })
+      .catch(err => {
+        console.log('error:', err);
+      });
+
+      console.log('Audio IBM Gerado!');
+}
+
+
+async function playAudio(){
+  var audio = new Audio('./src/audio/audio.mp3');
+  audio.play();
+}
+
+playAudio()
