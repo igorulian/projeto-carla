6d9a151df82464a367b9bc764803b42150d339d9
Igor Ulian <igorulian@Outlook.com>
2021-03-09 23:23:51 -0300

    IBM Watson txt to speec
    só falta dar play no audio

--- src/services/speak.js
+++ src/services/speak.js
@@ -1,10 +1,51 @@
-const say = require('say')
+const fs = require('fs')
+const dev = require('../services/log')
 
+require('dotenv').config()
+
+const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+// https://www.youtube.com/watch?v=rSjLe8k5DX0  https://cloud.ibm.com/apidocs/text-to-speech?code=node#data-collection
+
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+  apikey: "3383ucHfHBmBe1nhrOfSpIVOxUHRWBz-jfChAxdbW3sO",
+  }),
+  serviceUrl: "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/12575fac-d9f8-46c8-a251-f939cbfa69fa",
+  disableSslVerification: true, 
+});
 
 module.exports = {
-    say(text) {
-        say.speak(text, speed=10)
-        console.log(text)
+    async say(text) {
+
+      dev.log('Gerando audio...')
+
+      const synthesizeParams = {
+        text: '... ' + text + '',
+        accept: 'audio/mp3',
+        voice: 'pt-BR_IsabelaVoice'
+      };
+      
+      await textToSpeech.synthesize(synthesizeParams)
+        .then(response => {
+          // only necessary for wav formats,
+          // otherwise `response.result` can be directly piped to a file
+          return textToSpeech.repairWavHeaderStream(response.result);
+        })
+        .then(buffer => {
+          fs.writeFileSync('./src/audio/audio.mp3', buffer);
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
+
+        dev.log('Audio gerado!')
+
+        // Fazer tocar o audio
+        
+        dev.log('Audio tocado')
+      
+    
     }
 }
-
