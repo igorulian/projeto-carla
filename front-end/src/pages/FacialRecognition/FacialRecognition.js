5a66d81461d0a8944fc6b77cd7905340234127a4
Igor Ulian <igorulian@Outlook.com>
2021-03-30 02:19:37 -0300

    Frontend Video ""terminado""
    terminei +/- a parte do fronted de display de video, to só commitando para se der pau aqui no pc eu n perder tudo :)

--- front-end/src/pages/FacialRecognition/FacialRecognition.js
+++ front-end/src/pages/FacialRecognition/FacialRecognition.js
@@ -9,12 +9,7 @@ export default class VideoDisplay extends Component{
             <div>
                 <div className="page">
                     <CirculoCentral txtstyle={{fontSize: '14px', marginLeft: '7px', marginTop: '-12px'}} styles={{marginTop: '80px', width: '200px', height: '200px', marginLeft: '48%'}} lindafont='20px'/>
-                    <div style={{margin: '0 auto', width: '47%', paddingTop: '150px'}}>
-                        <iframe width="636" height="360" //id         ||
-                        style={{border: '1px solid #f7bb01',
-                        boxShadow:'0 0 10px #f7bb01'}}
-                        src="https://www.youtube.com/embed/VTtE6VyBpBE?autoplay=1&mute=1">
-                        </iframe> 
+                    <div style={{width: '636px',height: '360px',margin: '0 auto', width: '47%', paddingTop: '150px', border: '1px solid #f7bb01', boxShadow:'0 0 10px #f7bb01'}}>
                     </div>
                     <AudioFeedback/>
                 </div>
