c368a587e467ae5ec5aa8c538f1ae63395c72f8d
Igor Ulian <igorulian@outlook.com>
2021-07-30 19:20:05 -0300

    fixed whattime sentence

--- back-end/src/services/what-time.js
+++ back-end/src/services/what-time.js
@@ -4,7 +4,7 @@ import { say } from "../speak/speak.js"
 export async function WhatTime(command) {
     const data = getData()
 
-    await say(`Agora são ${data.hour} hora${data.hour > 1 ? 's' : ''} e ${data.minute} minuto${data.minute > 1 ? 's' : ''}`)
+    await say(`Agora são ${data.hour} hora${data.hour > 1 ? 's' : ''} e ${ data.minute > 0 ? `${data.minute} minuto${data.minute > 1 ? 's' : ''}` : ``}`)
 }
 
 function getData(){
