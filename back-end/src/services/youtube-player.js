8d59471cbd726b21bd38933f4117fc91878cfe53
Igor Ulian <igorulian@outlook.com>
2021-08-04 12:58:43 -0300

    add youtube api playlist seach

--- back-end/src/services/youtube-player.js
+++ back-end/src/services/youtube-player.js
@@ -1,19 +1,10 @@
-// // const Youtube = require('simple-youtube-api')
-// import Youtube from 'simple-youtube-api'
-// import dotenv from 'dotenv'
-// dotenv.config()
-
+import axios from "axios"
 import { hasConnections, PlayYoutubeVideo, StopYoutubeVideo } from "../socket/connection.js"
 import { say } from "../speak/speak.js"
+import dotenv from 'dotenv'
+import { response } from "express"
+dotenv.config()
 
-// import stream from 'youtube-audio-stream'
-// const decoder = require('lame').Decoder
-// import {Lame} from 'node-lame'
-// import Speaker from 'speaker'
-// const lame = new Lame()
-
-
-// // const youtubeApiKey = process.env.YOUTUBE_API_KEY.toString()
 
 export async function PlayMusic(command){
     if(!hasConnections())
@@ -21,8 +12,8 @@ export async function PlayMusic(command){
 
     const searchTerm = getSearchTerm(command)
     await say(`\n🎵 Tocando ${searchTerm}...`)
-    // const id = await getVideoBySearchTerm(searchTerm)
-    PlayYoutubeVideo('h6ru0vpISkE')
+    const id = await getPlaylistID(searchTerm)
+    PlayYoutubeVideo(id)
 }
 
 export async function StopMusic(command){
@@ -41,33 +32,9 @@ function getSearchTerm(command){
     return text
 }
 
-// // function stopVideo(){
-
-// // }
-
-
-// // async function getVideoBySearchTerm(searchTerm){
-// //     try{
-// //         const youtube = new Youtube('AIzaSyAaaKpZSGV36lUXD4VMmzLleyeFCqiE_lo')
-// //         const res = await youtube.searchVideos('' + searchTerm, 1)
-// //         return res[0]
-// //     }catch(err){
-// //         console.log(err)
-// //     }
-// // }
-// // async function playVideoBySearchTerm(searchTerm){
-// //     const link = await this.getVideoLinkBySearchTerm(searchTerm);
-// //     console.log("Tocando video no youtube (searchTerm), link: " + link)
-// //     // fazer tocar no front
-// //     // enviar provavelmente um socket para mudar de pagina sla
-// // }
-// // async function playVideoByLink(link){
-// //     // fazer tocar no front
-// //     // enviar provavelmente um socket para mudar de pagina sla
-// //     console.log("Tocando video no youtube (link), link:  " + link)
-// // }
-// // async function playVideoById(id){
-// //     // fazer tocar no front
-// //     // enviar provavelmente um socket para mudar de pagina sla
-// //     console.log("Tocando video no youtube (ID), link:  " + `https://youtube.com/watch?v=${id}`)
-// // }
+export async function getPlaylistID(searchTerm){
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_DATA_API_KEY}&part=snippet&q=${searchTerm}&type=playlist&maxResults=1`)
+    const playlist = response.data.items[0]
+    const id = playlist.id.playlistId
+    return id
+}
